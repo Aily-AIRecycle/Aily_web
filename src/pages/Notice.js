932333311a0cbd0aa00458e29b5d8813c8b50735
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-03-24 01:26:34 +0900

    initial commit

---
+++ src/pages/Notice.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+const Notice = () =>{
+    return <Fragment>
+        <h1>공지사항이에용</h1>
+        <ul>
+            <li>글제목</li>
+            <li>글제목</li>
+            <li>글제목</li>
+            <li>글제목</li>
+        </ul>
+    </Fragment>
+}
+
+export default Notice;
