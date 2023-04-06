4e91e9a9390cc1da0f432742474954b5ef7fcb06
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-04-06 19:34:03 +0900

    style: Main page section1
    반응형으로 만들기 위해 vw로 단위 변경
    미디어 쿼리 사용함

--- src/components/main/Section1.js
+++ src/components/main/Section1.js
@@ -1,13 +1,12 @@
 import classes from "./Section1.module.css";
-import { ReactComponent as Phone } from "../../img/main/phone.svg";
-
+import phone from "../../img/main/phone.svg";
 function Section1() {
   return (
     <div className={classes.box1}>
       <h1 className={classes.h1}>자연을 생각하는 분리수거 로봇</h1>
       <h2 className={classes.h2}>Aily를 통해 자동으로 분리수거를 해보세요.</h2>
       <div className={classes.space} />
-      <Phone />
+      <img src={phone} alt="phone" className={classes.phone} />
       <div className={classes.background} />
     </div>
   );
