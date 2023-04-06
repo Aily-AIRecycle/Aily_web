25597654f79ea3ec063cfaa3939398174edfa227
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-04-06 16:13:24 +0900

    style: Main header pc ver. complete
    추후에 변경될 수 있지만 현재로서는 완성함.

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -3,58 +3,57 @@ import { Link, NavLink } from "react-router-dom";
 
 const MainNavigation = () => {
   return (
-    <header className={classes.nav}>
-      <Link to="/">
-        <div className={classes.logo}>
-          <p>Ai Recycling</p>
-          <p>AiLY</p>
+    <header className={classes.head_wrap}>
+      <div className={classes.head}>
+        <div className={classes.head_logo}>
+          <Link to="/">
+            <p>Ai Recycling</p>
+            <p>AiLY</p>
+          </Link>
         </div>
-      </Link>
-      <ul className={classes.header}>
-        <li className={classes.menu}></li>
-        <li className={classes.menu}></li>
-        <li className={classes.menu}>
-          <NavLink
-            to="/board/notice"
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-          >
-            게시판
-          </NavLink>
-        </li>
-        <li className={classes.menu}>
-          <NavLink
-            to="/location"
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-          >
-            위치
-          </NavLink>
-        </li>
-        <li className={classes.menu}>
-          <NavLink
-            to="/isRecycle"
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-          >
-            {/* 재활용 가능 여부 검색 vs 재활용 여부 검색 */}
-            재활용 가능 여부 검색
-          </NavLink>
-        </li>
-        <li className={classes.menu}>
-          <NavLink
-            to="/login"
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-          >
-            로그인
-          </NavLink>
-        </li>
-      </ul>
+        <ul className={classes.head_menu}>
+          <li className={classes.menu}>
+            <NavLink
+              to="/board/notice"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              게시판
+            </NavLink>
+          </li>
+          <li className={classes.menu}>
+            <NavLink
+              to="/location"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              위치
+            </NavLink>
+          </li>
+          <li className={classes.menu}>
+            <NavLink
+              to="/isRecycle"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              재활용 여부 검색
+            </NavLink>
+          </li>
+          <li className={classes.menu}>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              로그인
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
