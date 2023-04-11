e06f48634b3fa69869708261c502cb9dc159d853
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-04-11 09:26:38 +0900

    feat: Add mobile menu bar
    모바일 메뉴바 기능 추가함
    애니메이션은 나중에 추가할 예정
    디자인 나중에 보완할 예정

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,61 +1,129 @@
+import { useState, useEffect } from "react";
 import classes from "./MainNavigation.module.css";
 import { Link, NavLink } from "react-router-dom";
+import bars from "../../src/img/header/bars-solid.svg";
+import x from "../../src/img/header/x-solid.svg";
 
-const MainNavigation = () => {
-  return (
-    <header className={classes.head_wrap}>
-      <div className={classes.head}>
-        <div className={classes.head_logo}>
-          <Link to="/">
-            <p>Ai Recycling</p>
-            <p>AiLY</p>
-          </Link>
+function MainNavigation() {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [menuShow, setMenuShow] = useState(false);
+
+  useEffect(() => {
+    const resizeListener = () => {
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", resizeListener);
+  });
+  console.log("innerWidth", innerWidth);
+
+  function menuHandler() {
+    setMenuShow(!menuShow);
+    console.log(menuShow);
+  }
+
+  let header;
+  if (innerWidth > 1000) {
+    header = (
+      <header className={classes.head_wrap}>
+        <div className={classes.head}>
+          <div className={classes.head_logo}>
+            <Link to="/">
+              <p>Ai Recycling</p>
+              <p>AiLY</p>
+            </Link>
+          </div>
+          <ul className={classes.head_menu}>
+            <li className={classes.menu}>
+              <NavLink
+                to="/board/notice"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                게시판
+              </NavLink>
+            </li>
+            <li className={classes.menu}>
+              <NavLink
+                to="/location"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                위치
+              </NavLink>
+            </li>
+            <li className={classes.menu}>
+              <NavLink
+                to="/isRecycle"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                재활용 여부 검색
+              </NavLink>
+            </li>
+            <li className={classes.menu}>
+              <NavLink
+                to="/login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                로그인
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <ul className={classes.head_menu}>
-          <li className={classes.menu}>
-            <NavLink
-              to="/board/notice"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              게시판
-            </NavLink>
-          </li>
-          <li className={classes.menu}>
-            <NavLink
-              to="/location"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              위치
-            </NavLink>
-          </li>
-          <li className={classes.menu}>
-            <NavLink
-              to="/isRecycle"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              재활용 여부 검색
-            </NavLink>
-          </li>
-          <li className={classes.menu}>
-            <NavLink
-              to="/login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              로그인
-            </NavLink>
-          </li>
-        </ul>
+      </header>
+    );
+  } else {
+    header = (
+      <div className={classes.head_wrap_mobile}>
+        <header className={classes.head_mobile}>
+          <div className={classes.head_logo_s}>
+            <Link to="/">
+              <p>Ai Recycling</p>
+              <p>AiLY</p>
+            </Link>
+          </div>
+          <img src={bars} alt="bars" onClick={menuHandler}></img>
+        </header>
+        {menuShow && (
+          <div className={classes.mobile_box}>
+            <img
+              src={x}
+              alt="x"
+              onClick={menuHandler}
+              className={classes.close}
+            />
+            <ul className={classes.head_menu_mobile}>
+              <li className={classes.menu_mobile}>
+                <Link to="/board/notice" onClick={menuHandler}>
+                  게시판
+                </Link>
+              </li>
+              <li className={classes.menu_mobile}>
+                <Link to="/location" onClick={menuHandler}>
+                  위치
+                </Link>
+              </li>
+              <li className={classes.menu_mobile}>
+                <Link to="/isRecycle" onClick={menuHandler}>
+                  재활용 여부 검색
+                </Link>
+              </li>
+              <li className={classes.menu_mobile}>
+                <Link to="/login" onClick={menuHandler}>
+                  로그인
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-    </header>
-  );
-};
-
+    );
+  }
+  return header;
+}
 export default MainNavigation;
