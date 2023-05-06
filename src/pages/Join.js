6e3a79d6da83937a1fd8074982e6b1b28d66e011
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-05-06 18:01:28 +0900

    style: 회원가입 페이지에 input tag 대신 클래스명 사용

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -7,19 +7,27 @@ function Join() {
         <h1 className={classes.title}>회원가입</h1>
         <div className={classes.join}>
           <form>
-            <input type="id" placeholder="아이디" />
-            <input type="password" placeholder="비밀번호" />
-            <input type="text" placeholder="이름" />
-            <input type="number" placeholder="년 (4자)" />
-            <input type="number" placeholder="월" />
-            <input type="number" placeholder="일" />
+            <input type="id" placeholder="아이디" className={classes.input} />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              className={classes.input}
+            />
+            <input type="text" placeholder="이름" className={classes.input} />
+            <input
+              type="number"
+              placeholder="년 (4자)"
+              className={classes.input}
+            />
+            <input type="number" placeholder="월" className={classes.input} />
+            <input type="number" placeholder="일" className={classes.input} />
             <select>
               <option selected>성별</option>
               <option value="M">남자</option>
               <option value="F">여자</option>
             </select>
-            <input placeholder="전화번호 입력" />
-            <input type="submit" value="회원가입" />
+            <input placeholder="전화번호 입력" className={classes.input} />
+            <input type="submit" value="회원가입" className={classes.input} />
           </form>
         </div>
       </main>
