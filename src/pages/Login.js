8b0e1e0bc933ed456afd266a9a3bee66c1afc298
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-05-12 17:53:28 +0900

    id에서 email로 바꿈

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,41 +7,41 @@ import axios from "axios";
 import useInput from "../hooks/use-input";
 
 function Login() {
-  const id = useInput("");
+  const email = useInput("");
   const password = useInput("");
 
   function loginHandler(event) {
     event.preventDefault();
 
     console.log("click login");
-    console.log("ID: ", id.value);
+    console.log("EMAIL: ", email.value);
     console.log("PW: ", password.value);
 
     axios
       .post("http://localhost:8080/member/login", {
-        id: id.value,
+        email: email.value,
         passwd: password.value,
       })
       .then((res) => {
         console.log(res);
-        console.log("res.data.id :: ", res.data.id);
+        console.log("res.data.email :: ", res.data.email);
         console.log("res.data.msg :: ", res.data.msg);
-        if (res.data.id === undefined) {
-          // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+        if (res.data.email === undefined) {
+          // email 일치하지 않는 경우 userId = undefined, msg = '입력하신 email이 일치하지 않습니다.'
           console.log("======================", res.data.msg);
-          alert("입력하신 id 가 일치하지 않습니다.");
-        } else if (res.data.id === null) {
-          // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
+          alert("입력하신 email이 일치하지 않습니다.");
+        } else if (res.data.email === null) {
+          // email 있지만, pw 는 다른 경우 userId = null , msg = undefined
           console.log(
             "======================",
             "입력하신 비밀번호 가 일치하지 않습니다."
           );
           alert("입력하신 비밀번호 가 일치하지 않습니다.");
-        } else if (res.data.id === id) {
-          // id, pw 모두 일치 userId = userId1, msg = undefined
+        } else if (res.data.email === email) {
+          // email, pw 모두 일치 userId = userId1, msg = undefined
           console.log("======================", "로그인 성공");
-          sessionStorage.setItem("user_id", id.value); // sessionStorage에 id를 user_id라는 key 값으로 저장
-          sessionStorage.setItem("name", res.data.name); // sessionStorage에 id를 user_id라는 key 값으로 저장
+          sessionStorage.setItem("user_email", email.value); // sessionStorage에 email를 user_email라는 key 값으로 저장
+          sessionStorage.setItem("name", res.data.name); // sessionStorage에 email를 user_email라는 key 값으로 저장
         }
         // 작업 완료 되면 페이지 이동(새로고침)
         document.location.href = "/";
@@ -62,9 +62,9 @@ function Login() {
               <input
                 className={classes.input}
                 type="id"
-                placeholder="아이디 또는 이메일"
-                value={id.value}
-                {...id}
+                placeholder="이메일"
+                value={email.value}
+                {...email}
               />
             </div>
             <div className={classes.div}>
