2d5668bf29dbdba0001d777f390913cef3e000e2
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-05-22 14:04:15 +0900

    Feat: 게시글 없을 때 표시 기능 추가

--- src/components/Board/BoardFilter.js
+++ src/components/Board/BoardFilter.js
@@ -1,9 +1,17 @@
 import BoardTitle from "./BoardTitle";
+import NoContent from "./NoContent";
 
 const category_type = { notice: 1, "q&a": 2, faq: 3, suggestion: 4 };
 
 function BoardFilter(props) {
   if (props.boardName !== undefined) {
+    if (
+      props.data.filter(
+        (article) => category_type[props.boardName] === article.category_id
+      ).length === 0
+    ) {
+      return <NoContent />;
+    }
     return props.data
       .filter(
         (article) => category_type[props.boardName] === article.category_id
@@ -16,6 +24,9 @@ function BoardFilter(props) {
         />
       ));
   } else {
+    if (props.data.length === 0) {
+      return <NoContent />;
+    }
     return props.data.map((article) => (
       <BoardTitle key={article.id} article={article} boardName={"entire"} />
     ));
