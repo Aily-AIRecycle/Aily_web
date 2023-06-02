0c71f6e1e7fb98fb9320741f619c44dab29ccf1e
dltkdgns00 <s20604@naver.com>
2023-06-02 17:28:51 +0900

    css replace *, commented api-key

--- src/components/Main/Section5.js
+++ src/components/Main/Section5.js
@@ -1,13 +1,15 @@
-import { apiKey } from "../../apikey";
+// import { apiKey } from "../../apikey";
 import classes from "./Section5.module.css";
 import { useCallback, useEffect, useRef } from "react";
 
-function Section5() {
+function Section5()
+{
   const mapElement = useRef(null);
 
   // 컴포넌트가 마운트될 때, 수동으로 스크립트를 넣어줍니다.
   // 이는 script가 실행되기 이전에 window.initMap이 먼저 선언되어야 하기 때문입니다.
-  const loadScript = useCallback((url) => {
+  const loadScript = useCallback((url) =>
+  {
     const firstScript = window.document.getElementsByTagName("script")[0];
     const newScript = window.document.createElement("script");
     newScript.src = url;
@@ -17,7 +19,8 @@ function Section5() {
   }, []);
 
   // script에서 google map api를 가져온 후에 실행될 callback 함수
-  const initMap = useCallback(() => {
+  const initMap = useCallback(() =>
+  {
     const { google } = window;
     if (!mapElement.current || !google) return;
 
@@ -36,21 +39,21 @@ function Section5() {
     });
   }, []);
 
-  useEffect(() => {
-    const script = window.document.getElementsByTagName("script")[0];
-    const includeCheck = script.src.startsWith(
-      "https://maps.googleapis.com/maps/api"
-    );
+  // useEffect(() => {
+  //   const script = window.document.getElementsByTagName("script")[0];
+  //   const includeCheck = script.src.startsWith(
+  //     "https://maps.googleapis.com/maps/api"
+  //   );
 
-    // script 중복 호출 방지
-    if (includeCheck) return initMap();
+  //   // script 중복 호출 방지
+  //   if (includeCheck) return initMap();
 
-    window.initMap = initMap;
-    // 본인 apiKey를 써야 함
-    loadScript(
-      `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap&language=en`
-    );
-  }, [initMap, loadScript]);
+  //   window.initMap = initMap;
+  //   // 본인 apiKey를 써야 함
+  //   loadScript(
+  //     `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap&language=en`
+  //   );
+  // }, [initMap, loadScript]);
 
   return (
     <div className={classes.box5}>
