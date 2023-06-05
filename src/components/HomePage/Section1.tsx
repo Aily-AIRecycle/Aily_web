f80ca9251309e65fd01c6bd9ae18d69c8bddf036
dltkdgns00 <s20604@naver.com>
2023-06-05 20:02:19 +0900

    refactoring complete

---
+++ src/components/HomePage/Section1.tsx
@@ -0,0 +1,15 @@
+import classes from "@/components/HomePage/styles/Section1.module.css";
+import phone from "img/main/phone.svg";
+function Section1()
+{
+  return (
+    <div className={classes.box1}>
+      <h1 className={classes.h1}>자연을 생각하는 분리수거 로봇</h1>
+      <h2 className={classes.h2}>Aily를 통해 자동으로 분리수거를 해보세요.</h2>
+      <img src="img/main/phone.svg" alt="phone" className={classes.phone} />
+      <div className={classes.background} />
+    </div>
+  );
+}
+
+export default Section1;
--- src/components/UI/ErrorText.js
+++
@@ -1,6 +0,0 @@
-import classes from "./ErrorText.module.css";
-function ErrorText(props) {
-  return <p className={classes.p}>{props.text}</p>;
-}
-
-export default ErrorText;
