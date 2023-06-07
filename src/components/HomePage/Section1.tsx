13dc165f4a97ed840932c819997dcc010bba0fb5
dltkdgns00 <s20604@naver.com>
2023-06-07 21:14:50 +0900

    completed Section1 & edit tailwind.config.js - add mediaquery

--- src/components/HomePage/Section1.tsx
+++ src/components/HomePage/Section1.tsx
@@ -1,13 +1,11 @@
-import classes from "@/components/HomePage/styles/Section1.module.css";
-import phone from "img/main/phone.svg";
 function Section1()
 {
   return (
-    <div className={classes.box1}>
-      <h1 className={classes.h1}>자연을 생각하는 분리수거 로봇</h1>
-      <h2 className={classes.h2}>Aily를 통해 자동으로 분리수거를 해보세요.</h2>
-      <img src="img/main/phone.svg" alt="phone" className={classes.phone} />
-      <div className={classes.background} />
+    <div className="grid place-items-center mt-[13.8vh] mq:mt-[10vh]">
+      <h1 className="text-5xl mb-[2.8vh] mq:text-3xl">자연을 생각하는 분리수거 로봇</h1>
+      <h2 className="text-2xl font-extralight text-[#767676] mb-[6.5vh] mq:text-lg">Aily를 통해 자동으로 분리수거를 해보세요.</h2>
+      <img src="img/main/phone1.png" alt="phone" className="h-[84vh]" />
+      <div className="bg-[#ffcab5] w-screen h-[63vh] mt-[-53vh] z-[-1]" />
     </div>
   );
 }
