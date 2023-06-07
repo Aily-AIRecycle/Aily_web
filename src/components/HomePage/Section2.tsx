dad6b9f3daf3011d3477c888fc3f4e7d194de66f
dltkdgns00 <s20604@naver.com>
2023-06-07 23:23:28 +0900

    completed Section2,3

--- src/components/HomePage/Section2.tsx
+++ src/components/HomePage/Section2.tsx
@@ -1,22 +1,17 @@
-import qrPhone from "img/main/qrcode.svg";
-import arrow from "img/main/right_left_arrow.svg";
-import recycleBin from "img/main/recycle_bin.svg";
-import classes from "@/components/HomePage/styles/Section2.module.css";
-
 function Section2()
 {
   return (
-    <div className={classes.box2}>
-      <h1 className={classes.h1}>QR 코드</h1>
-      <h2 className={classes.h2}>
+    <div className="grid place-items-center mt-[13.8vh] mq:mt-[10vh]">
+      <h1 className="text-5xl mb-[2.8vh] mq:text-3xl">QR 코드</h1>
+      <h2 className="text-2xl font-extralight text-[#726969] mb-[6.5vh] mq:text-lg">
         간편한 QR코드 인증으로 Aily 이용이 가능합니다.
       </h2>
-      <div className={classes.qrcode}>
-        <img src="img/main/qrcode.svg" alt="qrPhone" className={classes.qrPhone} />
-        <div className={classes.qrSpace} />
-        <img src="img/main/right_left_arrow.svg" alt="arrow" className={classes.arrow} />
-        <div className={classes.qrSpace} />
-        <img src="img/main/recycle_bin.svg" alt="recycleBin" className={classes.recycleBin} />
+      <div className="grid grid-cols-5 place-items-center mb-[13vh]">
+        <div></div>
+        <img src="img/main/qrphone.png" alt="qrPhone" className="h-[65vh]" />
+        <img src="img/main/right_left_arrow.svg" alt="arrow" className="h-[15.8vh]" />
+        <img src="img/main/AiLYre.png" alt="aily" className="h-[40.8vh]" />
+        <div></div>
       </div>
     </div>
   );
--- src/components/HomePage/Section3.tsx
+++ src/components/HomePage/Section3.tsx
@@ -1,27 +1,23 @@
-import classes from "@/components/HomePage/styles/Section3.module.css";
-import wallet from "img/main/wallet.svg";
-import cash from "img/main/cash.svg";
-import coin from "img/main/coin.svg";
-import bill from "img/main/money_bill.svg";
-
 function Section3()
 {
   return (
-    <div className={classes.box3}>
-      <h1 className={classes.h1}>포인트 제도</h1>
-      <div className={classes.description}>
-        <h2 className={classes.h2}>분리수거할 때 적립되는 포인트는&nbsp;</h2>
-        <h2 className={classes.h2}>현금처럼 사용이 가능합니다.</h2>
-      </div>
-      <div className={classes.point}>
-        <div className={classes.wallet_div}>
-          <img src="img/main/wallet.svg" alt="wallet" className={classes.wallet} />
-          <h2>홍길동님의 포인트</h2>
+    <div className="bg-[#ffe8df] w-screen text-center">
+      <div className="mt-[13.8vh] mq:mt-[10vh]">
+        <h1 className="text-5xl mb-[2.8vh] mq:text-3xl">포인트 제도</h1>
+        <div className="text-2xl font-extralight text-[#726969] mb-[6.5vh] mq:text-lg">
+          <h2 className="text-2xl font-extralight text-[#726969] mb-[6.5vh] mq:text-lg">분리수거할 때 적립되는 포인트는 현금처럼 사용이 가능합니다.</h2>
         </div>
-        <h1>22,000P</h1>
-        <img src="img/main/cash.svg" alt="cash" className={classes.cash} />
-        <img src="img/main/coin.svg" alt="coin" className={classes.coin} />
-        <img src="img/main/money_bill.svg" alt="bill" className={classes.bill} />
+        <div className="aspect-square w-1/3 bg-[#ffffff]">
+          <div className="">
+            <img src="img/main/wallet.svg" alt="wallet" className="" />
+            <h2>홍길동님의 포인트</h2>
+          </div>
+          <h1>22,000P</h1>
+
+        </div>
+        <img src="img/main/cash.svg" alt="cash" className="" />
+        <img src="img/main/coin.svg" alt="coin" className="" />
+        <img src="img/main/money_bill.svg" alt="bill" className="" />
       </div>
     </div>
   );
