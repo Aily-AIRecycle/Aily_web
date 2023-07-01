88530ad1f394aa6ef0c3819d3abf13c6404c56f5
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-07-01 15:11:57 +0900

    ⭐️Style: Change Header scss
    모바일 햄버거 메뉴 크기 변경
    css에서 scss로 변경
    width에 따라 헤더 변경 부분 windowWidth가 null일 경우 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
-import classes from "@/components/styles/Header.module.css";
-import HeaderList from "./HeaderList";
-import useWindowWidth from "../hooks/use-windowWidth";
+import classes from "@/components/styles/Header.module.scss";
+import HeaderList from "@/components/HeaderList";
+import useWindowWidth from "@/hooks/use-windowWidth";
 import Link from "next/link";
 import Image from "next/image";
 import bars from "img/header/bars-solid.svg";
@@ -10,7 +10,7 @@ import x from "img/header/x-solid.svg";
 import logo from "img/aily_logo.svg";
 
 function Header() {
-  const windowWidth: number = useWindowWidth();
+  const windowWidth = useWindowWidth();
   const [menuShow, setMenuShow] = useState<boolean>(false);
 
   function menuHandler() {
@@ -19,18 +19,13 @@ function Header() {
   }
 
   let header;
-  if (windowWidth > 1000) {
+  if (windowWidth === null) {
+  } else if (windowWidth > 1000) {
     header = (
       <header className={classes.head_wrap}>
         <div className={classes.head}>
           <Link href="/">
-            <Image
-              src={logo}
-              width={100}
-              height={100}
-              alt="logo"
-              className={classes.head_logo}
-            />
+            <Image src={logo} width={48} alt="logo" />
           </Link>
           <HeaderList ul={classes.head_menu} li={classes.menu} />
         </div>
@@ -49,13 +44,7 @@ function Header() {
               className={classes.head_logo_s}
             />
           </Link>
-          <Image
-            src={bars}
-            width={100}
-            height={100}
-            alt="bars"
-            onClick={menuHandler}
-          />
+          <Image src={bars} width={30} alt="bars" onClick={menuHandler} />
         </header>
         {menuShow && (
           <div className={classes.mobile_box}>
