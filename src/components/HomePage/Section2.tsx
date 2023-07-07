cf160736edc90fc94e75cb16a91bbfaeb9f87d6c
dltkdgns00 <s20604@naver.com>
2023-07-07 18:45:55 +0900

    mainpage reactive design completed, animation processing

--- src/components/HomePage/Section2.tsx
+++ src/components/HomePage/Section2.tsx
@@ -1,18 +1,52 @@
+import useIntersectionObserver from '@/hooks/user-intersectionObserver';
+import { useInView, useSpring, animated } from '@react-spring/web';
+import { useRef } from 'react';
+
 function Section2()
 {
+  const triggerRef = useRef<HTMLDivElement | null>(null);;
+  const dataRef = useIntersectionObserver(triggerRef, {
+    freezeOnceVisible: true
+  });
+  const [ref1, arrowtop] = useInView(() => ({
+    from: { x: "-50px" },
+    to: { x: dataRef?.isIntersecting ? "0px" : "0px" },
+  }))
+  const [ref2, arrowbottom] = useInView(() => ({
+    from: { x: "50px" },
+    to: { x: dataRef?.isIntersecting ? "0px" : "0px" },
+  }))
+  const [refqr, qrphone] = useInView(() => ({
+    config: {
+      duration: 4000,
+    },
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  }))
+  const [refai, AiLYre] = useInView(() => ({
+    config: {
+      duration: 4000,
+    }, from: { opacity: 0 },
+    to: { opacity: 1 },
+  }))
+
   return (
     <div className="w-screen aspect-[16/10]">
-      <div className="text-center mt-[13.8vh] mq:mt-[10vh]">
+      <div className="text-center mt-[13.8vh] mq:mt-[8vh]">
         <h1 className="text-5xl mb-[2.8vh] mq:text-3xl">QR 코드</h1>
         <h2 className="text-2xl font-extralight text-[#726969] mb-[6.5vh] mq:text-lg">
           간편한 QR코드 인증으로 Aily 이용이 가능합니다.
         </h2>
       </div>
-      <div className="items-center place-content-center w-screen flex aspect-[18/1] mb-[13.4vh]">
-        <img src="img/main/qrphone.png" alt="qrPhone" className=" h-[65vh]" />
-        <img src="img/main/right_left_arrow.svg" alt="arrow" className="h-[15.8vh] ml-[7.3vw] mr-[7.3vw]" />
-        <img src="img/main/AiLYre.png" alt="aily" className=" h-[40.8vh]" />
+      <div className="items-center place-content-center w-screen flex aspect-[18/1] mb-[13.4vh] mq:mb-[6vh]">
+        <animated.img ref={refqr} style={qrphone} src="img/main/qrphone.png" alt="qrPhone" className="w-[18vw] mq:w-[25vw]" />
+        <div className="w-[8.9vw] mq:w-[15vw] ml-[7.3vw] mr-[7.3vw]">
+          <animated.img ref={ref1} style={arrowtop} src="img/main/arrowtop.png" alt="arrowtop" />
+          <animated.img ref={ref2} style={arrowbottom} src="img/main/arrowbottom.png" alt="arrowbottom" />
+        </div>
+        <animated.img ref={refai} style={AiLYre} src="img/main/AiLYre.png" alt="aily" className="w-[23vw] mq:w-[30vw]" />
       </div>
+      <div ref={triggerRef} />
     </div>
   );
 }
--- src/components/HomePage/Section4.tsx
+++ src/components/HomePage/Section4.tsx
@@ -2,7 +2,7 @@ function Section4()
 {
   return (
     <div className="w-screen aspect-[16/10] text-center place-content-center">
-      <h1 className="text-5xl mt-[8.3vh] mb-[2.8vh] mq:text-3xl">위치 정보</h1>
+      <h1 className="text-5xl mt-[8.3vh] mq:mt-[8vh] mb-[2.8vh] mq:text-3xl">위치 정보</h1>
       <h2 className="text-2xl font-extralight text-[#726969] mb-[6.5vh] mq:text-lg">
         근처에 있는 Aily의 위치를 간편하게 확인하세요.
       </h2>
