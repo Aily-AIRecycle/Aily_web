9f988761fca76bf4518a8277d86c1b2b80ede67d
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2023-07-20 15:32:12 +0900

    Chart Implementation
    General, PET, can rate graph (circular donut graph), total weight bar graph of general, PET, can according to machine number
    *Donut graph goes into the admin dashboard

---
+++ src/app/statistics/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+// Import required libraries
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Bar } from "react-chartjs-2";
+import Header from "@/components/UI/Header";
+import Footer from "@/components/UI/Footer";
+import { registerables, Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from 'chart.js';
+import 'chartjs-adapter-moment';
+import BarGraph from "@/components/StatisStics/BarGraph";
+import LineGraph from "@/components/StatisStics/LineGraph";
+
+const Statistics = () => {
+  return (
+    <>
+      <Header />
+      <BarGraph />
+      <LineGraph />
+      <Footer />
+    </>
+  );
+  }
+export default Statistics;
