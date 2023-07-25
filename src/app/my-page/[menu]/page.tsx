2a730e9177fd89ba78c01020a7e4de84d1ad5db1
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-07-25 12:06:06 +0900

    Member withdrawal page refactoring, design improvement

--- src/app/my-page/[menu]/page.tsx
+++ src/app/my-page/[menu]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Dashboard from "@/components/MyPage/Dashboard";
-import Edit from "@/components/MyPage/Edit";
 import Leave from "@/components/MyPage/Leave";
+import Edit from "@/components/MyPage/Edit";
 
 const MyPage = () => {
   const pathname = usePathname();
+
   return (
     <>
       {pathname === "/my-page/dashboard" && <Dashboard />}
