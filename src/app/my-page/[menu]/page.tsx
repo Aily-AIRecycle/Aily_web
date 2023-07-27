ada86141c2c2d1c05215140e05250b36f3394df8
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-07-27 17:51:37 +0900

    Feat: Add changepassword page
    Complete except for getting password from server

--- src/app/my-page/[menu]/page.tsx
+++ src/app/my-page/[menu]/page.tsx
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation";
 import Dashboard from "@/components/MyPage/Dashboard";
 import Leave from "@/components/MyPage/Leave";
 import Edit from "@/components/MyPage/Edit";
+import ChangePassword from "@/components/MyPage/ChangePassword";
 
 const MyPage = () => {
   const pathname = usePathname();
@@ -11,6 +12,7 @@ const MyPage = () => {
     <>
       {pathname === "/my-page/dashboard" && <Dashboard />}
       {pathname === "/my-page/edit" && <Edit />}
+      {pathname === "/my-page/password" && <ChangePassword />}
       {pathname === "/my-page/leave" && <Leave />}
     </>
   );
---
+++ src/components/MyPage/ChangePassword.tsx
@@ -0,0 +1,90 @@
+import { FormEvent } from "react";
+import useInput from "@/hooks/use-input";
+import classes from "@/components/MyPage/styles/ChangePassword.module.scss";
+import SubmitButton from "../UI/SubmitButton";
+import useFormValidation from "@/hooks/use-formValidation";
+import { FormData, Errors, ChangeHandler } from "@/hooks/use-formValidation";
+import ErrorText from "@/components/UI/ErrorText";
+import axios from "axios";
+
+export default function ChangePassword() {
+  const password = useInput("");
+  const checkNewPassword = useInput("");
+
+  const passwordRegex =
+    /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+
+  const validationRules = {
+    password: (value: string) => passwordRegex.test(value),
+  };
+  
+  const [formData, errors, onChangeHandler]: [FormData, Errors, ChangeHandler] =
+    useFormValidation(validationRules);
+
+  const changePasswordHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // 서버에서 가져온 비밀번호와 같으면 비밀번호 바꿈
+    if(errors.password){
+        alert("비밀번호 형식이 맞지 않습니다.");
+    } else if (formData.password !== checkNewPassword.value) {
+      alert("바꾸려는 비밀번호가 일치하지 않습니다.");
+    }  else{
+        axios.post("", {}).then(() => {
+          
+        });
+    }
+  };
+
+  return (
+    <>
+      <div className={classes.box}>
+        <p className={classes.title}>비밀번호 변경</p>
+        <form onSubmit={changePasswordHandler}>
+          <ul>
+            <li className={classes.data}>
+              <label htmlFor="password">현재 비밀번호</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="비밀번호를 변경하려면 현재 비밀번호를 입력해주세요."
+                value={password.value}
+                onChange={password.onChange}
+              />
+            </li>
+            <li className={classes.data}>
+              <label htmlFor="check_password">새 비밀번호</label>
+              <input
+                type="password"
+                placeholder=" 새 비밀번호"
+                name="password"
+                value={formData.password}
+                onChange={onChangeHandler}
+              />
+            </li>
+            <li className={classes.data}>
+              <label htmlFor="check_password">새 비밀번호 확인</label>
+              <input
+                type="password"
+                placeholder=" 새 비밀번호 확인"
+                name="check_password"
+                value={checkNewPassword.value}
+                onChange={checkNewPassword.onChange}
+              />
+            </li>
+          </ul>
+          <div className={classes.errors}>
+            {errors.password && (
+              <ErrorText text="8~16자의 영문 대/소문자, 숫자, 특수문자를 사용해 주세요." />
+            )}
+            {formData.password.trim() &&
+              formData.password !== checkNewPassword.value && (
+                <ErrorText text="비밀번호가 일치하지 않습니다." />
+              )}
+          </div>
+          <SubmitButton value="회원 탈퇴" />
+        </form>
+      </div>
+    </>
+  );
+}
