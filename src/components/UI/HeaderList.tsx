1c18b79955e2e17e52577fa9c4cd052ae0fb923b
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-08-02 13:30:32 +0900

    Update HeaderList.tsx
    마이페이지에 접속하면 대시보드 먼저 보이게 변경

--- src/components/UI/HeaderList.tsx
+++ src/components/UI/HeaderList.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 function HeaderList(props: any) {
-
   const handleLogout = () => {
     // Perform logout logic, such as clearing session data
     sessionStorage.clear();
@@ -16,49 +15,33 @@ function HeaderList(props: any) {
   return (
     <ul className={props.ul}>
       <li className={props.li}>
-        <Link href="/board/all">
-          게시판
-        </Link>
+        <Link href="/board/all">게시판</Link>
       </li>
       <li className={props.li}>
-        <Link href="/location">
-          위치
-        </Link>
+        <Link href="/location">위치</Link>
       </li>
       <li className={props.li}>
-        <Link href="/isRecycle">
-          재활용 여부 검색
-        </Link>
+        <Link href="/isRecycle">재활용 여부 검색</Link>
       </li>
       <li className={props.li}>
-        <Link href="/statistics">
-          통계
-        </Link>
+        <Link href="/statistics">통계</Link>
       </li>
       {sessionName === "ok" && (
         <>
           <li className={props.li}>
-            <Link href="/my-page/mypage">
-              내 정보
-            </Link>
+            <Link href="/my-page/dashboard">내 정보</Link>
           </li>
           <li className={props.li} onClick={handleLogout}>
-            <Link href="/">
-            로그아웃
-            </Link>
+            <Link href="/">로그아웃</Link>
           </li>
           {sessionID === "테스트" && (
-            <Link href="/hidden">
-            관리자 대쉬보드
-            </Link>
+            <Link href="/hidden">관리자 대쉬보드</Link>
           )}
         </>
       )}
       {sessionName !== "ok" && (
         <li className={props.li}>
-          <Link href="/login">
-            로그인
-          </Link>
+          <Link href="/login">로그인</Link>
         </li>
       )}
     </ul>
