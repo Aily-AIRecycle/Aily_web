464e0452fa3bc8f4875dbca05e514cd013835120
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2023-08-09 21:11:19 +0900

    User Infromation ok
    User Infromation All ok

--- src/components/MyPage/Profile.tsx
+++ src/components/MyPage/Profile.tsx
@@ -1,19 +1,52 @@
+"use client";
+import classes from "@/components/MyPage/styles/Point.module.scss";
+import axios from "axios";
+import wallet from "img/main/wallet.svg";
 import Image from "next/image";
-import classes from "@/components/MyPage/styles/Profile.module.scss";
+import { useEffect, useState } from "react";
 
-export default function Profile(props: any) {
+export default function Point() {
+  interface Data {
+    GEN: number;
+    CAN: number;
+    profile: string;
+    phonenumber: string;
+    nickname: string;
+    point: number;
+    PET: number;
+  }
+
+  const [userName, setUserName] = useState<string | null>(null);
+  const [data, setData] = useState<Data | null>(null);
+
+  useEffect(() => {
+    const name = sessionStorage.getItem("name") || localStorage.getItem("name");
+    setUserName(name);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .post("/member/member/mypage", {
+        nickname:
+          sessionStorage.getItem("name") || localStorage.getItem("name"),
+      })
+      .then((response) => {
+        // Handle the POST request response
+        setData(response.data);
+      })
+      .catch((error) => {
+        // Handle the POST request error
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className={classes.img}>
-      {props.src && (
-        <Image
-          src={props.src}
-          priority
-          width={180}
-          height={180}
-          alt="profile_img"
-          style={{ borderRadius: "50%" }}
-        />
-      )}
+    <div className={classes.box}>
+      <div>
+        <Image src={wallet} alt="wallet" width={36} />
+        <p>{userName}님의 포인트</p>
+      </div>
+      {data && <p className={classes.point}>{data.point}P</p>}
     </div>
   );
 }
