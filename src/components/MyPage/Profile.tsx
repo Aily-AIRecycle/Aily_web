68ba091e3131e583feaf3c870cd4db026286e06a
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2023-08-09 21:35:35 +0900

    fixed+
    fixed+

--- src/components/MyPage/Profile.tsx
+++ src/components/MyPage/Profile.tsx
@@ -1,52 +1,55 @@
-"use client";
-import classes from "@/components/MyPage/styles/Point.module.scss";
-import axios from "axios";
-import wallet from "img/main/wallet.svg";
-import Image from "next/image";
+import Image, { ImageLoader } from "next/image";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function Point() {
-  interface Data {
-    GEN: number;
-    CAN: number;
-    profile: string;
-    phonenumber: string;
-    nickname: string;
-    point: number;
-    PET: number;
-  }
+import classes from "@/components/MyPage/styles/Profile.module.scss";
 
-  const [userName, setUserName] = useState<string | null>(null);
-  const [data, setData] = useState<Data | null>(null);
+const imageLoader: ImageLoader = ({ src, width, quality }) => {
+  return `${src}?cache=${Date.now()}`;
+};
+export default function Profile(props: any) {
+  const [imageUrl1, setImageUrl1] = useState<string>("");
 
-  useEffect(() => {
-    const name = sessionStorage.getItem("name") || localStorage.getItem("name");
-    setUserName(name);
-  }, []);
-
-  useEffect(() => {
+  const fetchProfileImage = () => {
     axios
-      .post("/member/member/mypage", {
-        nickname:
-          sessionStorage.getItem("name") || localStorage.getItem("name"),
+      .post("/member/member/userimage", {
+        phonenumber: sessionStorage.getItem("phone_number"),
       })
       .then((response) => {
-        // Handle the POST request response
-        setData(response.data);
+        const relativePath = response.data.split("/member/image/")[1];
+        const imageUrl = "/member/image/" + relativePath;
+        setImageUrl1(imageUrl);
       })
       .catch((error) => {
-        // Handle the POST request error
-        console.error(error);
+        // Handle errors if needed
       });
-  }, []);
+  };
+
+  useEffect(() => {
+    // Fetch the profile image initially
+    fetchProfileImage();
+
+    // Set up a periodic timer to refresh the profile image every 5 minutes (adjust as needed)
+    const refreshInterval = setInterval(fetchProfileImage, 5 * 60 * 1000);
+
+    // Clean up the timer on component unmount
+    return () => {
+      clearInterval(refreshInterval);
+    };
+  }, []); // Run only once on component mount
 
   return (
-    <div className={classes.box}>
-      <div>
-        <Image src={wallet} alt="wallet" width={36} />
-        <p>{userName}님의 포인트</p>
-      </div>
-      {data && <p className={classes.point}>{data.point}P</p>}
+    <div className={classes.img}>
+      {imageUrl1 && (
+        <Image
+          loader={imageLoader}
+          src={imageUrl1}
+          width={180}
+          height={180}
+          alt="profile_img"
+          className={classes.profileImg}
+        />
+      )}
     </div>
   );
 }
