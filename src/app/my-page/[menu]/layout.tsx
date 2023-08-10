0fedde733776b9fd7120fe7fe1b768c580abe060
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-08-10 13:09:19 +0900

    Update my-page/[menu]/layout.tsx and page.tsx

--- src/app/my-page/[menu]/layout.tsx
+++ src/app/my-page/[menu]/layout.tsx
@@ -1,10 +1,3 @@
-"use client";
-import MyPageNavigation from "@/components/MyPage/MyPageNavigation";
-import Footer from "@/components/UI/Footer";
-import Header from "@/components/UI/Header";
-import store from "@/store";
-import { Provider } from "react-redux";
-
 export const metadata = {
   title: "Aily - 마이페이지",
 };
@@ -14,16 +7,5 @@ export default function MyPageLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Provider store={store}>
-        <Header />
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <MyPageNavigation />
-          {children}
-        </div>
-        <Footer />
-      </Provider>
-    </>
-  );
+  return <>{children}</>;
 }
