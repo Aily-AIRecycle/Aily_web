3013254918ad021fe8ccac11a3ec9bc338d79c29
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-08-24 16:47:35 +0900

    Chart scss -> tailwind css, add box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       screens: {
-        'mq': { 'raw': '(max-width: 800px)' },
-      }
+        mq: { raw: "(max-width: 800px)" },
+      },
+      boxShadow: {
+        myBoxShadow:
+          "0px 4px 12px 0px rgba(28, 64, 97, 0.06), 0px 12px 36px 0px rgba(51, 82, 100, 0.1)",
+      },
     },
   },
   plugins: [],
-}
+};
