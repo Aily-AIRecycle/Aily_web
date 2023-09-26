49f6afe23f44bcf9a26fa88c1bed44ac8f8da8a2
SangHoon Lee <s20604@naver.com>
2023-09-26 20:37:46 +0900

    section6 rearrange

--- src/components/HomePage/Section6.tsx
+++ src/components/HomePage/Section6.tsx
@@ -1,14 +1,13 @@
 function Section6()
 {
     return (
-        <div className="w-full relative">
-            <div className="grid m-auto bg-[#FFF9F6] place-items-center">
-                <div className="w-1/5 mq:w-1/3 inline-block">
-                    <a href='https://play.google.com/store/apps/details?id=com.yoonchan.aily&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img className="" alt='다운로드하기 Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/ko_badge_web_generic.png' /></a>
-                </div>
-                <div className="inline-block w-1/3">
-                    <img src="img/main/PlaystoreQR.png" />
-                </div>
+        <div className="w-full m-auto bg-[#FFF9F6] pb-[5vw] grid place-items-center">
+            <div className="inline-block w-1/5 mq:w-1/3">
+                <img className="rounded-xl mq:rounded-md" src="img/main/PlaystoreQR.png" />
+            </div>
+            <div className="h-[2vh]"></div>
+            <div className="w-1/5 mq:w-1/3">
+                <a href='https://play.google.com/store/apps/details?id=com.yoonchan.aily&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='다운로드하기 Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/ko_badge_web_generic.png' /></a>
             </div>
         </div>
     )
